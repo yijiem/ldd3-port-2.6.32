3a386b1cbb28e0c6dc9ee72b8ba1969c27ca95a7
Yijie Ma <yijiema1991@gmail.com>
2020-05-31 13:38:38 -0400

    Add misc-progs.patch

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ${DIR}
 # Apply patches
 patch -p0<patches/scull.patch
+patch -p0<patches/misc-progs.patch
 
 # Build
 cd scull
+make
+
+cd ${DIR}
+cd misc-progs
 make
